3071f0845227f5543b98a3fe47ad105b8fa55a43
Sarah Sophie Stieß <sarah.stiess@iste.uni-stuttgart.de>
2024-06-20 08:25:46 +0200

    refactor event from managed system.

--- org.palladiosimulator.analyzer.slingshot.networking.test/src/org/palladiosimulator/analyzer/slingshot/networking/test/messages/ManagedSystemTimeEventMessage.java
+++ org.palladiosimulator.analyzer.slingshot.networking.test/src/org/palladiosimulator/analyzer/slingshot/networking/test/messages/ManagedSystemTimeEventMessage.java
@@ -1,19 +1,25 @@
 package org.palladiosimulator.analyzer.slingshot.networking.test.messages;
 
+import java.util.Set;
+
 import org.palladiosimulator.analyzer.slingshot.networking.data.EventMessage;
+import org.palladiosimulator.analyzer.slingshot.networking.test.messages.ManagedSystemTimeEventMessage.PlanStep;
 
 /**
  *
  * @author Sarah Stieß
  *
  */
-public class ManagedSystemTimeEventMessage extends EventMessage<Double> {
+public class ManagedSystemTimeEventMessage extends EventMessage<PlanStep> {
 
-    public static final String MESSAGE_MAPPING_IDENTIFIER = ManagedSystemTimeEventMessage.class.getSimpleName();
+    public static final String MESSAGE_MAPPING_IDENTIFIER = "PlanStepApplied";
 
-    public ManagedSystemTimeEventMessage(final Double payload, final String creator) {
+    public ManagedSystemTimeEventMessage(final PlanStep payload, final String creator) {
         super(MESSAGE_MAPPING_IDENTIFIER, payload, creator);
-	}
+    }
 
+    public record PlanStep(Double pointInTime, Set<String> policies) {
+
+    }
 }
 
